7001e99d6db2b4405d7a948065a8398735130050
Lucas Mendonça <lucas21rio@gmail.com>
2022-02-09 01:13:22 -0300

    menu links

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -7,8 +7,8 @@ export default function Menu() {
 
     return (
         <div className="menu">
-            <a className="social" href="https://google.com"><BsYoutube size={24} color="#fff" /></a>
-            <a className="social" href="https://google.com"><BsInstagram size={24} color="#fff" /></a>
+            <a className="social" target="_blank" href="https://youtube.com"><BsYoutube size={24} color="#fff" /></a>
+            <a className="social" target="_blank" href="https://instagram.com"><BsInstagram size={24} color="#fff" /></a>
             <Link className="menu-item" to="/links">Meus Links</Link>
         </div>
     )
